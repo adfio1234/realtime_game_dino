b763cd2ee17c128165fbf34e7ae6066a754b1343
adfio1234 <dlwlsdnr5246@gmail.com>
2024-10-08 20:39:39 +0900

    test를 위한 커밋

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World");
-// })
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
 
 initSocket(server);
 
