5a02e8b909f7502ad28d270c79c9a4a3528f9b42
adfio1234 <dlwlsdnr5246@gmail.com>
2024-10-11 21:26:24 +0900

    console 출력문 추가

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));//서버에 정적 파일 서빙
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("Hello World");
